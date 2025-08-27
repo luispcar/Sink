1526ced1cef04acc339fefa085e45575d70c897c
Luis Cardena <65931358+luispcar@users.noreply.github.com>
2025-08-26 22:59:10 -0500

    change error page

--- app/error.vue
+++ app/error.vue
@@ -1,13 +1,3 @@
-<script setup>
-import errorImage from './assets/images/404.svg?raw'
-</script>
-
 <template>
-  <NuxtLayout name="default">
-    <a
-      class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center [&_svg]:w-full"
-      href="/"
-      v-html="errorImage"
-    />
-  </NuxtLayout>
+  <h1>This Link Does Not Exist</h1>
 </template>
